07239f178e6582028aa59d8b5d9b637569ce21e5
Anatolii Borysov <borisano@gmail.com>
2018-04-25 14:26:40 +0300

    Add cart and add dummy data to it

--- public/script.js
+++ public/script.js
@@ -1,3 +1,20 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      {title: 'item 1', price: 1},
+      {title: 'item 2', price: 2},
+      {title: 'item 3', price: 3}
+    ],
+    cart: []
+  },
+  methods: {
+    addItem: function(index){
+      var item = this.items[index];
+
+      this.total += item.price;
+      this.cart.push(item);
+    }
+  }
 });

97a751aa98492eadb40baa871fdbcaeaa7f2206c
Anatolii Borysov <borisano@gmail.com>
2018-04-26 15:51:09 +0300

    Add currency filter

--- public/script.js
+++ public/script.js
@@ -3,9 +3,9 @@ new Vue({
   data: {
     total: 0,
     items: [
-      {id: 1, title: 'item 1', price: 1},
-      {id: 2, title: 'item 2', price: 2},
-      {id: 3, title: 'item 3', price: 3}
+      {id: 1, title: 'item 1', price: 9.09},
+      {id: 2, title: 'item 2', price: 2.45},
+      {id: 3, title: 'item 3', price: 3.21}
     ],
     cart: []
   },
@@ -25,11 +25,18 @@ new Vue({
         this.cart.push({
           id: item.id,
           title: item.title,
+          price: item.price,
           quantity: 1
         });
       }
 
       this.total += item.price;
+    }
+  },
+  filters: {
+    currency: function(price) {
+
+      return '$'+ price.toFixed(2);
     }
   }
 });
